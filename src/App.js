5861751a915df822c827f661ab1d12f6ff5585ee
Ibrahim <123ibrahim7890@gmail.com>
2022-12-12 17:17:15 +0500

    formik and yup 1st guide

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SimpleInput from './components/SimpleInput';
+import BasicForm from './components/BasicForm'
+import MeraInput from "./components/MeraInput"
+function App() {
+  return (
+    <div className="app">
+        <MeraInput></MeraInput>
+      {/* <SimpleInput />
+      <BasicForm/> */}
+    </div>
+  );
+}
+
+export default App;
